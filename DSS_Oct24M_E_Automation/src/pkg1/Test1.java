57ee25136f50cd8d796c67855bd7b0bc778c2201
sandipdalavi1111 <sandipdalavi1111@gmail.com>
2025-01-26 15:06:14 +0530

    commit1

---
+++ DSS_Oct24M_E_Automation/src/pkg1/Test1.java
@@ -0,0 +1,22 @@
+package pkg1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	public static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("123");
+		System.out.println("234");
+	}
+
+}
